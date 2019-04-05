8095445a0027e87d4bb680596e400b11f640a84e
Robert <robertkoziolek4c@gmail.com>
2019-04-05 18:07:05 +0200

    first commit

---
+++ src/main/java/com/robcio/ccnotepad/CcNotepadApplication.java
@@ -0,0 +1,13 @@
+package com.robcio.ccnotepad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcNotepadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcNotepadApplication.class, args);
+	}
+
+}
