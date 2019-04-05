82c8c1059275737f344c00f3c523b461b76a5099
Robert <robertkoziolek4c@gmail.com>
2019-04-05 19:18:54 +0200

    Reverted to a lower version for some quite annoying reasons

--- src/main/java/com/robcio/ccnotepad/CcNotepadApplication.java
+++ src/main/java/com/robcio/ccnotepad/CcNotepadApplication.java
@@ -2,12 +2,17 @@ package com.robcio.ccnotepad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CcNotepadApplication {
+public class CcNotepadApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CcNotepadApplication.class);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(CcNotepadApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CcNotepadApplication.class, args);
+    }
 }
