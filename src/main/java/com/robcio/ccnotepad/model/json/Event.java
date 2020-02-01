994be00738ce87a7d00a53d31ecf928d96662d06
Robert <robertkoziolek4c@gmail.com>
2020-02-01 19:01:50 +0100

    JSON timezone fix

--- src/main/java/com/robcio/ccnotepad/model/json/Event.java
+++ src/main/java/com/robcio/ccnotepad/model/json/Event.java
@@ -1,5 +1,6 @@
 package com.robcio.ccnotepad.model.json;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,6 +19,7 @@ public class Event {
     private String filmId;
     private String cinemaId;
     private String businessDay;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Europe/Warsaw")
     private Date eventDateTime;
     private Set<String> attributeIds;
     private String bookingLink;
